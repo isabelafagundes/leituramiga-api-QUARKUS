69c08a0c5125ae67ac8318fbf209fcdd16d56c22
isabelafagundes <isabelahidalgo.2004@gmail.com>
2024-09-05 21:26:39 -0300

    Adicionando referência a tabela de cidade no livro

--- src/main/resources/db/scripts/livro-v1.sql
+++ src/main/resources/db/scripts/livro-v1.sql
@@ -1,21 +1,6 @@
 -- liquibase formatted sql
 -- changeset auth:livro-v1 splitStatements:true endDelimiter:;
 
-CREATE TABLE IF NOT EXISTS usuario
-(
-    email_usuario      INTEGER PRIMARY KEY,
-    usarname           VARCHAR(30),
-    nome               VARCHAR(40),
-    senha              VARCHAR(60),
-    celular            VARCHAR(11),
-    descricao          VARCHAR(120),
-    url_imagem         VARCHAR(256),
-    codigo_instituicao BIGINT NOT NULL,
-    codigo_endereco    BIGINT NOT NULL
-);
-
-
-
 CREATE TABLE IF NOT EXISTS livro_solicitacao
 (
     codigo    INTEGER PRIMARY KEY,
@@ -26,12 +11,6 @@ CREATE TABLE IF NOT EXISTS categoria
 (
     codigo_categoria INTEGER PRIMARY KEY,
     descricao        VARCHAR(120)
-);
-
-CREATE TABLE IF NOT EXISTS status_livro
-(
-    codigo_status_livro INTEGER PRIMARY KEY,
-    descricao           VARCHAR(80)
 );
 
 CREATE TABLE IF NOT EXISTS livro
@@ -45,10 +24,9 @@ CREATE TABLE IF NOT EXISTS livro
     codigo_ultima_solicitacao BIGINT       NOT NULL,
     codigo_categoria          BIGINT       NOT NULL,
     codigo_status_livro       BIGINT       NOT NULL,
-
+    codigo_cidade            BIGINT       NOT NULL,
+    FOREIGN KEY (codigo_cidade) REFERENCES cidade (codigo_cidade),
     FOREIGN KEY (email_usuario) REFERENCES usuario (email_usuario),
     FOREIGN KEY (codigo_ultima_solicitacao) REFERENCES livro_solicitacao (codigo),
     FOREIGN KEY (codigo_categoria) REFERENCES categoria (codigo_categoria),
-    FOREIGN KEY (codigo_status_livro) REFERENCES status_livro (codigo_status_livro)
-
 );
